0dc886b69617d5243df246d2e1530543dd80490a
Balaji Pathak <balajipathak@startbitsolutions.com>
2025-07-01 12:26:28 +0530

    bakend code with ts

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
 import app from './app';
 import dotenv from 'dotenv';
+import { exec } from 'child_process';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+exec('npx prisma db push', (err, stdout, stderr) => {
+  if (err) {
+    console.error('❌ Failed to sync Prisma schema with DB:', err);
+  } else {
+    console.log('✅ Prisma schema synced with DB');
+    console.log(stdout);
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  }
 });
